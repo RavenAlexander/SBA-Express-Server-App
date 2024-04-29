26286663872521b2337d7d7eb4fcb6042c7299d6
ralexander <theravenalexander@gmail.com>
2024-04-29 13:18:12 -0600

    adding browser views

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SBA-Express-Server-App
+
+This project features a homemade API that tracks sample user data. The idea is that you can view recipes that users have posted as well as rate them, or leave comments.
+
+## Objectives
+- Create a server application with Node and Express.
+- Create a RESTful API using Express.
+- Create Express middleware.
+- Use Express middleware.
+- Use a template engine to render views with Express.
+- Interact with a self-made API through HTML forms.
+
+# Languages and Tools Used
+HTML, JS, Node.JS, Express, EJS (view engine)
+
+## Disclaimer:
+If you click on "Download Recipe" you won't get an actual recipe. Instead you'll get an image of a cat. Enjoy
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -9,19 +8,110 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public')); //to allow static files
+app.use(express.static(__dirname + '/public')); //middleware to allow static files
+
+const users = require("./routes/users");
+const posts = require("./routes/posts");
+const ratings = require("./routes/rate");
+
+const error = require("./utilities/error");
 
 
 //index.ejs template
 app.get("/", (req, res) => {
   const title = { name: "Recipe Viewer"};
-  res.render("pages/index", { title: title } );
+  const posts = [
+    {
+      id: 1,
+      userId: 1,
+      title: "Recipe 1",
+      content:
+        "similique esse doloribus nihil accusamus\nomnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\nperspiciatis cum ut laudantium\nomnis aut molestiae vel vero",
+    },
+    {
+      id: 2,
+      userId: 1,
+      title: "Recipe 2",
+      content:
+        "eum sed dolores ipsam sint possimus debitis occaecati\ndebitis qui qui et\nut placeat enim earum aut odit facilis\nconsequatur suscipit necessitatibus rerum sed inventore temporibus consequatur",
+    },
+    {
+      id: 3,
+      userId: 1,
+      title: "Recipe 3",
+      content:
+        "non et quaerat ex quae ad maiores\nmaiores recusandae totam aut blanditiis mollitia quas illo\nut voluptatibus voluptatem\nsimilique nostrum eum",
+    },
+    {
+      id: 4,
+      userId: 2,
+      title: "Recipe 4",
+      content:
+        "odit magnam ut saepe sed non qui\ntempora atque nihil\naccusamus illum doloribus illo dolor\neligendi repudiandae odit magni similique sed cum maiores",
+    },
+    {
+      id: 5,
+      userId: 2,
+      title: "Recipe 5",
+      content:
+        "alias dolor cumque\nimpedit blanditiis non eveniet odio maxime\nblanditiis amet eius quis tempora quia autem rem\na provident perspiciatis quia",
+    },
+    {
+      id: 6,
+      userId: 2,
+      title: "Recipe 6",
+      content:
+        "debitis eius sed quibusdam non quis consectetur vitae\nimpedit ut qui consequatur sed aut in\nquidem sit nostrum et maiores adipisci atque\nquaerat voluptatem adipisci repudiandae",
+    },
+    {
+      id: 7,
+      userId: 3,
+      title: "Recipe 7",
+      content:
+        "deserunt eos nobis asperiores et hic\nest debitis repellat molestiae optio\nnihil ratione ut eos beatae quibusdam distinctio maiores\nearum voluptates et aut adipisci ea maiores voluptas maxime",
+    },
+    {
+      id: 8,
+      userId: 3,
+      title: "Recipe 8",
+      content:
+        "rerum ut et numquam laborum odit est sit\nid qui sint in\nquasi tenetur tempore aperiam et quaerat qui in\nrerum officiis sequi cumque quod",
+    },
+    {
+      id: 9,
+      userId: 3,
+      title: "Recipe 9",
+      content:
+        "ea velit perferendis earum ut voluptatem voluptate itaque iusto\ntotam pariatur in\nnemo voluptatem voluptatem autem magni tempora minima in\nest distinctio qui assumenda accusamus dignissimos officia nesciunt nobis",
+    },
+  ];
+  res.render("pages/index", { title: title , posts: posts } );
 });
 
 // users.ejs template
 app.get("/users", (req, res) => {
   const title = {name: "User List"};
-  res.render("pages/users", {title: title});
+  const users = [
+    {
+      id: 1,
+      name: "Carey",
+      username: "cyare23",
+      email: "cy23@example.com",
+    },
+    {
+      id: 2,
+      name: "Mikoto",
+      username: "Miiko",
+      email: "mikoto_u@example.com",
+    },
+    {
+      id: 3,
+      name: "Ronald",
+      username: "RonRonRon",
+      email: "mronald@example.com",
+    },
+  ];
+  res.render("pages/users", {title: title, users: users});
 });
 
 //rate.ejs template
@@ -32,14 +122,9 @@ app.get("/rate", (req, res) => {
 
 
 app.get("/image", (req, res) => {
-  res.download('./image/cat.jpeg')
+  res.download('./public/image/cat.jpeg')
 });
 
-const users = require("./routes/users");
-const posts = require("./routes/posts");
-const ratings = require("./routes/rate");
-
-const error = require("./utilities/error");
 
 
 // Time-Logging Middlewaare
